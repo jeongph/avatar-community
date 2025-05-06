8c851175af2f2d346ecea26c1b85ff17be7fc08e
JeongUk Park <dnr1105@gmail.com>
2025-05-06 23:13:00 +0900

    feat: init from start.spring.io

---
+++ src/main/java/me/jeonguk/avatar_community/AvatarCommunityApplication.java
@@ -0,0 +1,13 @@
+package me.jeonguk.avatar_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvatarCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvatarCommunityApplication.class, args);
+	}
+
+}
