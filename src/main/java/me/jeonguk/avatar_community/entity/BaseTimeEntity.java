10a99474af8f61397f22170fca3f965544f05f17
JeongUk Park <dnr1105@gmail.com>
2025-05-23 21:25:25 +0900

    feat: 생성시각, 업데이트시각 관련 BaseEntity.java 추가

---
+++ src/main/java/me/jeonguk/avatar_community/entity/BaseTimeEntity.java
@@ -0,0 +1,12 @@
+package me.jeonguk.avatar_community.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+}
